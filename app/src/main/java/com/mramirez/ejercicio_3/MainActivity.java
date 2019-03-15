48bfa0c26f7535279df59d562d40ddc37b581c80
Marvin Ramirez <00120717@>
2019-03-15 15:30:54 -0600

    Se agregaron mas colores

--- app/src/main/java/com/mramirez/ejercicio_3/MainActivity.java
+++ app/src/main/java/com/mramirez/ejercicio_3/MainActivity.java
@@ -2,12 +2,25 @@ package com.mramirez.ejercicio_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout ly1,ly2,ly3;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ly1 = findViewById(R.id.ly1);
+        ly2 = findViewById(R.id.ly2);
+        ly3 = findViewById(R.id.ly3);
+
+
+
     }
 }
