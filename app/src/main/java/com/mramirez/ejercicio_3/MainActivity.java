dd47c67a74202317eb7ebfcecbd19e4e4c165c02
Marvin Ramirez <00120717>
2019-03-16 16:46:36 +0100

    Se modifico MainActivity

--- app/src/main/java/com/mramirez/ejercicio_3/MainActivity.java
+++ app/src/main/java/com/mramirez/ejercicio_3/MainActivity.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
